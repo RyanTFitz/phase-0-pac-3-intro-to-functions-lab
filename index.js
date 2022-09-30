7adcd0c19135027cd2bf07b2fc34bc803002232b
Ryan Fitzpatrick <ryantfitzpatrick@yahoo.com>
2022-09-30 13:56:44 -0400

    commit tree 947d031b2ebd16540628d5939514abccc4d77cd9

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    return string.toUpperCase(); 
+} 
+
+function whisper(string) {
+    return string.toLowerCase(); 
+}
+
+function logShout(string) {
+    console.log(`HELLO`); 
+}
+sayHello.toUpperCase(); 
+
+function logWhisper(string) {
+    console.log(`hello`)
+}
+sayHello.toLowerCase(); 
+
+function sayHiToHeadphonedRoommate(string) {
+    return `${'I can\'t hear you!'}`
+}
+sayHiToHeadphonedRoommate.toLowerCase(); 
